1657bc829aaf300b7ed38b6c5d647c983cfd5011
Nathan Huff <nrhuff@umn.edu>
2012-11-15 16:13:13 -0600

    Setup puppetdb

--- puppet-up.pp
+++ puppet-up.pp
@@ -1,6 +1,10 @@
 node default {
   include apache
   include apache::mod_ssl
+  class{'puppetdb':
+    manage_redhat_firewall => false,
+    database => 'embedded',
+  }
 
   apache::conf_snippet{'proxy':
     content => "LoadModule proxy_module modules/mod_proxy.so\nLoadModule proxy_http_module modules/mod_proxy_http.so\n",
@@ -14,12 +18,18 @@ node default {
   unicorn::instance{'master1':
     basedir     => '/etc/puppet/master1',
     port        => '18140',
+    subscribe   => Puppet::Server::Instance['master1'],
   }
   $puppet_port = '8140'
   $unicorn_port = '18140'
   $puppet_vardir = '/var/lib/puppet'
   apache::vhost{'pupppet-master1':
     content => template('puppet/apache_vhost.erb'),
+  }
+
+  puppet::server::puppetdb_config{'master1':
+    configdir => '/etc/puppet/master1',
+    notify    => Unicorn::Instance['master1'],
   }
   Yumrepo<| |> -> Package<| |>
   yumrepo{'ahc':
